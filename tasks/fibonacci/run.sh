17c621f40fb1309a396260b96c9a196158fc65d9
Eric020615 <92671386+Eric020615@users.noreply.github.com>
2022-06-03 10:12:37 +0800

    Update run.sh

--- tasks/fibonacci/run.sh
+++ tasks/fibonacci/run.sh
@@ -5,9 +5,9 @@ mkfifo iopipe0 iopipe1
 
 if [[ $VERBOSE ]]
 then
-    timeout $TIMELIMIT <iopipe0 java -classpath solutions/fibonacci/Fibonacci | tee iopipe1 & <iopipe1 python3 tasks/fibonacci/run.py | tee iopipe0;
+    timeout $TIMELIMIT <iopipe0 java -classpath solutions Fibonacci | tee iopipe1 & <iopipe1 python3 tasks/fibonacci/run.py | tee iopipe0;
 else
-    timeout $TIMELIMIT <iopipe0 java -classpath solutions/fibonacci/Fibonacci | python3 tasks/fibonacci/run.py > iopipe0;
+    timeout $TIMELIMIT <iopipe0 java -classpath solutions Fibonacci | python3 tasks/fibonacci/run.py > iopipe0;
 fi
 PASS=$?
 
