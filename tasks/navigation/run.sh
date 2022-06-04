3382dec03199fad410c1b1f588ff584f017cd07d
Eric020615 <92671386+Eric020615@users.noreply.github.com>
2022-06-04 11:00:23 +0800

    Update run.sh

--- tasks/navigation/run.sh
+++ tasks/navigation/run.sh
@@ -6,9 +6,9 @@ mkfifo iopipe0 iopipe1
 
 if [[ $VERBOSE ]]
 then
-    timeout $TIMELIMIT <iopipe0 java -classpath solutions Navigation | tee iopipe1 & <iopipe1 python3 tasks/navigation/run.py | tee iopipe0;
+    timeout $TIMELIMIT <iopipe0 java -classpath solutions $SOLUTION_FILE | tee iopipe1 & <iopipe1 python3 tasks/navigation/run.py | tee iopipe0;
 else
-    timeout $TIMELIMIT <iopipe0 java -classpath solutions Navigation | python3 tasks/navigation/run.py > iopipe0;
+    timeout $TIMELIMIT <iopipe0 java -classpath solutions $SOLUTION_FILE | python3 tasks/navigation/run.py > iopipe0;
 fi
 PASS=$?
 
