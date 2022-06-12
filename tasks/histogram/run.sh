c5628445afac6b17fed47769ad7469674614741e
Junbin9 <96436605+Junbin9@users.noreply.github.com>
2022-06-12 23:10:45 +0800

    Update run.sh

--- tasks/histogram/run.sh
+++ tasks/histogram/run.sh
@@ -5,9 +5,9 @@ mkfifo iopipe0 iopipe1
 
 if [[ $VERBOSE ]]
 then
-    timeout $TIMELIMIT <iopipe0 java -classpath solutions histogram | tee iopipe1 & <iopipe1 python3 tasks/histogram/run.py | tee iopipe0;
+    timeout $TIMELIMIT <iopipe0 java -classpath solutions histogram/histogram | tee iopipe1 & <iopipe1 python3 tasks/histogram/run.py | tee iopipe0;
 else
-    timeout $TIMELIMIT <iopipe0 java -classpath solutions histogram | python3 tasks/histogram/run.py > iopipe0;
+    timeout $TIMELIMIT <iopipe0 java -classpath solutions histogram/histogram | python3 tasks/histogram/run.py > iopipe0;
 fi
 PASS=$?
 
